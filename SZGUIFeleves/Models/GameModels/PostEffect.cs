e9dd3e88523c2970cc37da90191b0eb843665bf0
kurinczkydd <kurinczkydd@gmail.com>
2022-03-19 14:51:28 +0100

    PostEffect static class created

---
+++ SZGUIFeleves/Models/GameModels/PostEffect.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SZGUIFeleves.Models
+{
+    public static class PostEffect
+    {
+        public static void ApplyBloom(ref byte[] screenArray, Vec2d windowSize, int intensity)
+        {
+
+        }
+    }
+}
