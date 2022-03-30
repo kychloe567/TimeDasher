3aa449023902bdff3f48a86d313073f858a3d7a5
DavidGgly <gergelydavid00@gmail.com>
2022-03-30 16:57:07 +0200

    Created Player.cs
    Contains few basic properties

---
+++ SZGUIFeleves/Models/DrawableObjects/Player.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SZGUIFeleves.Models.DrawableObjects
+{
+    public class Player : DrawableObject
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double SpeedX { get; set; }
+        public double SpeedY { get; set; }
+        
+
+        public override bool Equals(object obj)
+        {
+            if (obj != null && obj is Player && (obj as Player).Id == this.Id)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public override DrawableObject GetCopy()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Vec2d GetMiddle()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool Intersects(DrawableObject d)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool IsVisible(Camera camera)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
