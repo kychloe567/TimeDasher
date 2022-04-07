44cf2db5885b2a7350547b7f9696394e202a880d
BudaiDaniel <budai.daniel03@gmail.com>
2022-04-07 20:01:57 +0200

    Trap is now child of Rectangle (might be better with Drawable Object tho)

--- SZGUIFeleves/Models/DrawableObjects/Trap.cs
+++ SZGUIFeleves/Models/DrawableObjects/Trap.cs
@@ -4,37 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using SZGUIFeleves.Helpers;
+using SZGUIFeleves.Models.DrawableObjects;
 
 namespace SZGUIFeleves.Models
 {
-    public class Trap : DrawableObject
+    public class Trap : Rectangle
     {
-        public Vec2d Size { get; set; }
-        public double Right
-        {
-            get { return Position.x + Size.x; }
-        }
-        public double Bottom
-        {
-            get { return Position.y + Size.y; }
-        }
-        public override Vec2d GetMiddle()
-        {
-            return new Vec2d((Position.x + Size.x / 2), (Position.y + Size.y / 2));
-        }
-        public override bool Equals(object obj)
-        {
-            throw new NotImplementedException();
-        }
-
-        public override DrawableObject GetCopy()
-        {
-            throw new NotImplementedException();
-        }
-
 
         public override bool Intersects(DrawableObject d)
         {
+            ;
             if (d is Circle c)
             {
                 double closestX = MathHelper.Clamp(Position.x, Right, c.Position.x);
@@ -68,14 +47,16 @@ namespace SZGUIFeleves.Models
 
                 return intersects;
             }
-            else if (d is Player j)
-            {
-                j.Dies();
-                return true;
-            }
+           
             else if (d is Rectangle r)
             {
                 return !(r.Position.x >= Right || r.Right <= Position.x || r.Position.y >= Bottom || r.Bottom <= Position.y);
+            }
+            else if (d is Player j)
+            {
+                
+                j.Dies();
+                return true;
             }
             else if (d is Polygon p)
             {
@@ -89,10 +70,6 @@ namespace SZGUIFeleves.Models
             return false;
         }
 
-        public override bool IsVisible(Camera camera)
-        {
-            throw new NotImplementedException();
-        }
-
+  
     }
 }
