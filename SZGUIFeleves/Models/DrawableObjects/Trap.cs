39a45b72d9077fe6d4fe202a29d3075c546f3e88
budaidaniel <budai.daniel03@gmail.com>
2022-04-17 14:02:13 +0200

    Creared Trap class with basic properties

---
+++ SZGUIFeleves/Models/DrawableObjects/Trap.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SZGUIFeleves.Models.DrawableObjects
+{
+    public class Trap : Rectangle
+    {
+        public double SpeedX { get; set; }
+        public double SpeedY { get; set; }
+        public bool IsMoving { get; set; }
+
+
+    }
+}
